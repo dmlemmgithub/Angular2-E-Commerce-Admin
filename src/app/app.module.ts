851382b00b048144b9ebafc75ef7f03f2121c81e
David <david.lmd84@gmail.com>
2017-11-26 16:30:06 +0100

    Antes de instalar material ( npm install --save @angular/material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MaterialModule } from './modules/material/material.module';
+import { LoginService } from './services/login.service';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavBarComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MaterialModule,
+    HttpModule, routing
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
